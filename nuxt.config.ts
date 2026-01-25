981fd3a649315cc29f20662b045e58a2ab6839ea
ShaDow <yvesfynt24@gmail.com>
2026-01-25 18:33:12 +0100

    migration vers nuxt v1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
+  nitro:{
+    preset: 'static',
+    compressPublicAssets: true
+  },
   modules: [
     '@nuxt/icon', 
     '@nuxt/image',
@@ -11,6 +15,30 @@ export default defineNuxtConfig({
     '@nuxtjs/robots', 
     '@nuxtjs/tailwindcss'
   ],
+  app:{
+    head:{
+      htmlAttrs:{lang: 'fr'},
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      titleTemplate: '%s . Ultimate Pronos',
+      script:[
+        {
+          id:'Cookiebot',
+          src:'https://consent.cookiebot.com/uc.js',
+          async: true,
+          'data-cbid':"b433eb58-7ffb-431f-84b2-e83a7e017c20"
+        },
+      ],
+      meta:[
+        {name: "robots", content:'index, follow'},
+        {name: "theme-color", content:'#000000'}
+      ],
+      link:[
+        {rel: 'icon', type: 'img/png' , href: '/logo.png', sizes: ' 96x96'},
+        {rel: 'canonical' , href: ''}
+      ]
+    }
+  },
   i18n:{
     strategy: 'prefix',
     defaultLocale:'fr',
@@ -31,7 +59,22 @@ export default defineNuxtConfig({
   sitemap:{
     autoLastmod:true
   },
+  experimental:{
+    payloadExtraction: true,
+    inlineRouteRules:false
+  },
+  routeRules:{
+    '/':{prerender:true},
+    '/fr/**':{prerender:true},
+    'en/**':{prerender:true}
+  },
+  sitemap:{
+
+    i18n:true,
+    gzip: true
+  },
   robots:{
     rules:[{ userAgent: '*' , allow: '/'}]
   }
+
 })
